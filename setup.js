1457bbe9ae85892a68d4eceb8af54ee86f273b10
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-11 21:09:44 +1100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,3 +1,6 @@
 title @s title §cMurder §4Mystery
 title @s subtitle §l§asetup successfully
-tellraw @s {"rawtext":[{"text":"Thank you for downloading my script!\nYou can use "/function mm/start" to start a new round!\nHave fun and enjoy!"}]}
+tellraw @s {"rawtext":[{"text":"Thank you for downloading my script!\nYou can use "/function mm/start" to start a new round!\nHave fun and enjoy!\n\n§cNOTE: §l§4This may affeect blocks placed at the coordinates: 0, 0, 0!"}]}
+scoreboard objectives add mmDB dummy
+structure load setup 0 0 0
+tickingarea add circle 2 2 2 3 mm.spawn
