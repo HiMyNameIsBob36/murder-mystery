740691cbc30c889e5dbd850a83e74d4dad222bb8
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-11 21:38:54 +1100

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,6 +1,12 @@
+# Universal
 title @a title Your role is...
 tellraw @a {"rawtext":[{"text":"Role Description:"}]}
+# Imposters
 tag @r[tag=queue] add imposter
+tag @r[tag=queue] add vanish
 tag @a[tag=imposter] remove queue
-title @a[tag=imposter] subtitle §c§lIMPOSTER
-tellraw @a[tag=imposter] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper is "},{"selector":"@a"}]}
+tag @a[tag=vanish] remove queue
+title @a[tag=imposter] subtitle §l§cIMPOSTER
+title @a[tag=imposter] subtitle §l§cIMPOSTER §r§o(Vanish)
+tellraw @a[tag=imposter] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=vanish]"},{"text":"§aYou can hold your item to vanish for 25 seconds!"]}
+tellraw @a[tag=vanish] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=imposter]"}]}
