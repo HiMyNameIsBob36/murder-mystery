d0148436f1e677f21fbd418ea19c880b87e2567b
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-11 21:45:22 +1100

    Update roles.js

--- roles.js
+++ roles.js
@@ -8,5 +8,10 @@ tag @a[tag=imposter] remove queue
 tag @a[tag=vanish] remove queue
 title @a[tag=imposter] subtitle §l§cIMPOSTER
 title @a[tag=imposter] subtitle §l§cIMPOSTER §r§o(Vanish)
-tellraw @a[tag=imposter] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=vanish]"},{"text":"§aYou can hold your item to vanish for 25 seconds!"}]}
+tellraw @a[tag=imposter] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=vanish]"}]}
+tellraw @a[tag=vanish] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=imposter]"},{"text":"§aYou can hold your custom item to vanish for 25 seconds!"}]}
+# Netural (Lawyer)
+tag @r[tag=queue] add lawyer
+tag @a[tag=lawyer] remove queue
+title @a[tag=lawer] subtitle §l§9LAWYER
 tellraw @a[tag=vanish] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper(s) is §4"},{"selector":"@a[tag=imposter]"}]}
