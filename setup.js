3c8f61e118b4524aef97a687861ded5b75b0d700
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-11 20:58:37 +1100

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,3 @@
+title @s title §cMurder §4Mystery
+title @s subtitle §l§asetup successfully
+tellraw @s {"rawtext":[{"text":"Thank you for downloading my script!\nYou can use "/function mm/start" to start a new round!\nHave fun and enjoy!"}]}
