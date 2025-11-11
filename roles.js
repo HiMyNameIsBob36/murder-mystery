7d2ba6a2c36c2f230a659ce4a50ab7dfcaa07d81
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2025-11-11 21:25:48 +1100

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,4 +1,6 @@
-
+title @a title Your role is...
+tellraw @a {"rawtext":[{"text":"Role Description:"}]}
 tag @r[tag=queue] add imposter
 tag @a[tag=imposter] remove queue
-tellraw @a {"rawtext":[{"text":""}]}
+title @a[tag=imposter] subtitle §c§lIMPOSTER
+tellraw @a[tag=imposter] {"rawtext":[{"text":"§cYou need to kill all remaining inocents, your helper is "},{"selector":"@a"}]}
